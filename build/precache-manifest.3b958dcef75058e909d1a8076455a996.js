2df7a2e3dbe9bf3b0985f63d08ddf6aba61d7645
blaze021 <tarunblaze@gmail.com>
2020-07-23 00:30:26 +0530

    The build folder has been added

---
+++ build/precache-manifest.3b958dcef75058e909d1a8076455a996.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b70b5efc90d53647975758180ccf3fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "134c69983a7bfd0f0d9d",
+    "url": "/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "82add5157ef80a3b441f",
+    "url": "/static/css/main.fdcb629b.chunk.css"
+  },
+  {
+    "revision": "134c69983a7bfd0f0d9d",
+    "url": "/static/js/2.7eb3d9b4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7eb3d9b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82add5157ef80a3b441f",
+    "url": "/static/js/main.44433620.chunk.js"
+  },
+  {
+    "revision": "20c019d97ad4c64c6ab7",
+    "url": "/static/js/runtime-main.002cd011.js"
+  },
+  {
+    "revision": "d50f40b9e75613b521d1bf03a0543b1e",
+    "url": "/static/media/sign_up.d50f40b9.jpg"
+  }
+]);
